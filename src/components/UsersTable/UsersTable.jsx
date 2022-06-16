5a4f1582d794b699907b9aad0e908c339f5a2857
Liia Kozlenko <l.kozlenko@infinet.ru>
2022-06-16 11:22:22 +0500

    Добавлена сортировка

--- src/components/UsersTable/UsersTable.jsx
+++ src/components/UsersTable/UsersTable.jsx
@@ -1,32 +1,43 @@
+import React from 'react';
 import { Table } from 'antd';
 import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 
-function UsersTable({data}) {  
-
-  function sort(a, b) {
-    return a - b;
+class UsersTable extends React.Component {  
+  constructor(props) {
+    super(props);
+    this.state = { };
   }
 
-  const columns = [
+  sort = (a, b) => {
+    a = a || '';
+    b = b || '';
+
+    return a.localeCompare(b);
+  };
+
+  getName = (user) => {
+    return `${user.first_name} ${user.last_name}`;
+  }
+
+  columns = [
     {
       title: 'Пользователь',
       dataIndex: 'username',
       key: 'username',
+      sorter: (a, b) => this.sort(a.username, b.username),
     },
     {
       title: 'ФИО',
       key: 'first_name',
-      render: (data) => (
-        data.profile_link ? 
-          <a href={data.profile_link}>{`${data.first_name} ${data.last_name}`}</a> : 
-          `${data.first_name} ${data.last_name}`
-      ),
+      render: (user) => user.profile_link ? <a href={user.profile_link}>{this.getName(user)}</a> : this.getName(user),
+      sorter: (a, b) => this.sort(this.getName(a), this.getName(b)),
     },
     {
       title: 'Email',
       key: 'email',      
-      render: (data) => <a href={`mailto:${data.email}`}>{data.email}</a>
+      render: (data) => <a href={`mailto:${data.email}`}>{data.email}</a>,
+      sorter: (a, b) => this.sort(a.email, b.email),
     },
     {
       title: 'Оплачено',
@@ -36,9 +47,11 @@ function UsersTable({data}) {
     },
   ];
   
-  return (
-    <Table dataSource={data} columns={columns} bordered/>
-  );
+  render() {
+    return (
+      <Table dataSource={this.props.data} columns={this.columns} bordered/>
+    );
+  }
 }
 
 export default UsersTable;
